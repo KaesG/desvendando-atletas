7eb046b5be965f44ddc8dd6ea8d40603c5d09635
Estella Karinna <estella.karinna@gmail.com>
2025-01-09 19:08:55 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o munod com o seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens. ",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com sua força, determinação e talento, ela coleciona medalhas em competições nacionais e internacionais, incluindo os Jogos Olímpicos. ",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    },
+
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destacou em competições internacionais. Com sua técnica apurada e determinação, ela conquistou diversas medalhas e se tornou um dos principais nomes do judô brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    }
+];
